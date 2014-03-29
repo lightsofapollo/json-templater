bd936b12fab3dd07978b9476f94b044d1eddb620
James Lal <james@lightsofapollo.com>
2014-03-29 16:52:41 -0700

    Object clone/replace implementation

--- string_test.js
+++ string_test.js
@@ -2,66 +2,75 @@ suite('string', function() {
   var assert = require('assert');
   var subject = require('./string');
 
-  suite('#replace', function() {
+  function verify(title, input, output) {
+    test(title, function() {
+      var result = subject.apply(subject, input);
+      assert.equal(output, result);
+    });
+  }
 
-    function verify(title, input, output) {
-      test(title, function() {
-        var result = subject.apply(subject, input);
-        assert.equal(output, result);
-      });
-    }
+  verify('no token', ['woot', {}], 'woot');
 
-    verify('no token', ['woot', {}], 'woot');
+  verify(
+    'token no value',
+    ['{{token}}', {}],
+    '{{token}}'
+  );
 
-    verify(
-      'token no value',
-      ['{{token}}', {}],
-      '{{token}}'
-    );
+  verify(
+    'one token',
+    ['{{token}}', { token: 'v' }],
+    'v'
+  );
 
-    verify(
-      'one token',
-      ['{{token}}', { token: 'v' }],
-      'v'
-    );
+  verify(
+    'nested token',
+    ['{{foo.bar.baz}}', { foo: { bar: { baz: 'baz' } } }],
+    'baz'
+  );
 
-    verify(
-      'nested token',
-      ['{{foo.bar.baz}}', { foo: { bar: { baz: 'baz' } } }],
-      'baz'
-    );
+  verify(
+    'nested token no value',
+    ['{{foo.bar}}', { foo: {} }],
+    '{{foo.bar}}'
+  );
 
-    verify(
-      'nested token no value',
-      ['{{foo.bar}}', { foo: {} }],
-      '{{foo.bar}}'
-    );
+  verify(
+    'replace in the middle of string',
+    [
+      'foo bar {{baz}} qux',
+      { baz: 'what?' }
+    ],
+    'foo bar what? qux'
+  );
 
-    verify(
-      'replace in the middle of string',
-      [
-        'foo bar {{baz}} qux',
-        { baz: 'what?' }
-      ],
-      'foo bar what? qux'
-    );
+  verify(
+    'multiple replacements',
+    [
+      '{{a}} {{woot}} bar baz {{yeah}}',
+      { a: 'foo', yeah: true }
+    ],
+    'foo {{woot}} bar baz true'
+  );
 
-    verify(
-      'multiple replacements',
-      [
-        '{{a}} {{woot}} bar baz {{yeah}}',
-        { a: 'foo', yeah: true }
-      ],
-      'foo {{woot}} bar baz true'
-    );
+  verify(
+    'object notation for non-object',
+    [
+      '{{a.b.c}} what',
+      { a: { b: 'iam b' } }
+    ],
+    '{{a.b.c}} what'
+  );
 
-    verify(
-      'object notation for non-object',
-      [
-        '{{a.b.c}} what',
-        { a: { b: 'iam b' } }
-      ],
-      '{{a.b.c}} what'
-    );
-  });
+  verify(
+    'array view',
+    [
+      'do what {{fields.0}}',
+      {
+        fields: ['first']
+      }
+    ],
+    'do what first'
+  );
+
 });
